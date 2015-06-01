0a97a253ef10146c2850f9f05c33e8d7112987cb
Lauri Hakko <lauri.hakko@gmail.com>
2015-06-01 11:25:41 +0300

    print the image right way

--- pbm2tupper.go
+++ pbm2tupper.go
@@ -15,8 +15,8 @@ func main() {
 	bound := imgfile.Bounds()
 	if str == "pbm" {
 		if bound.Max.X == 106 && bound.Max.Y == 17 {
-			for x := 0; x < 106; x = x + 1 {
-				for y := 0; y < 17; y = y + 1 {
+			for y := 0; y < 17; y = y + 1 {
+				for x := 0; x < 106; x = x + 1 {
 					b, _, _, _ := imgfile.At(x, y).RGBA()
 					if b == 0 {
 						print(0)
